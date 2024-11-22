d985f23aac801fb313d42ebc6b9a80c35f780860
yannpinard <yannpinard2000@gmail.com>
2024-11-22 11:09:28 +0100

    Ajout 4 boutons

--- projetCollaboratif_STM32/Core/Src/main.c
+++ projetCollaboratif_STM32/Core/Src/main.c
@@ -1,9 +1,9 @@
 /* USER CODE BEGIN Header */
 /**
- ******************************************************************************
+ *******************************************************************************
  * @file           : main.c
  * @brief          : Main program body
- ******************************************************************************
+ *******************************************************************************
  * @attention
  *
  * Copyright (c) 2024 STMicroelectronics.
@@ -13,7 +13,7 @@
  * in the root directory of this software component.
  * If no LICENSE file comes with this software, it is provided AS-IS.
  *
- ******************************************************************************
+ *******************************************************************************
  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
@@ -29,55 +29,124 @@
 /* USER CODE BEGIN Includes */
 #include "../../../Drivers/BSP/STM32412G-Discovery/stm32412g_discovery.h"
 #include "../../../Drivers/BSP/STM32412G-Discovery/stm32412g_discovery_lcd.h"
+//#include "stm32412g_discovery_io.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+typedef struct {
+    uint16_t x;        // Position X
+    uint16_t y;        // Position Y
+    uint16_t width;    // Largeur
+    uint16_t height;   // Hauteur
+    uint32_t color;    // Couleur par défaut
+    uint32_t selectedColor; // Couleur en surbrillance
+} Button;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
 /* USER CODE BEGIN PV */
-
+Button buttons[4];          // Tableau pour les 4 boutons
+uint8_t selectedButton = 0; // Indice du bouton sélectionné
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+void InitializeButtons();
+void HighlightButton(uint8_t index);
+void HandleJoystick();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+/**
+ * @brief Initialisation des boutons à l'écran LCD
+ */
+void InitializeButtons() {
+    // Définir les positions, dimensions et couleurs des boutons
+    buttons[0] = (Button){40, 60, 80, 40, LCD_COLOR_BLUE, LCD_COLOR_RED};
+    buttons[1] = (Button){160, 60, 80, 40, LCD_COLOR_BLUE, LCD_COLOR_RED};
+    buttons[2] = (Button){40, 140, 80, 40, LCD_COLOR_BLUE, LCD_COLOR_RED};
+    buttons[3] = (Button){160, 140, 80, 40, LCD_COLOR_BLUE, LCD_COLOR_RED};
+
+    // Dessiner les boutons
+    for (uint8_t i = 0; i < 4; i++) {
+        BSP_LCD_SetTextColor(buttons[i].color);
+        BSP_LCD_FillRect(buttons[i].x, buttons[i].y, buttons[i].width, buttons[i].height);
+    }
+    // Mettre le premier bouton en surbrillance
+    HighlightButton(0);
+}
+
+/**
+ * @brief Met en surbrillance un bouton spécifique
+ * @param index L'indice du bouton à surligner
+ */
+void HighlightButton(uint8_t index) {
+    for (uint8_t i = 0; i < 4; i++) {
+        if (i == index) {
+            BSP_LCD_SetTextColor(buttons[i].selectedColor);
+        } else {
+            BSP_LCD_SetTextColor(buttons[i].color);
+        }
+        BSP_LCD_FillRect(buttons[i].x, buttons[i].y, buttons[i].width, buttons[i].height);
+    }
+}
+
+/**
+ * @brief Gère les interactions avec le joystick
+ */
+void HandleJoystick() {
+    JOYState_TypeDef joystickState = BSP_JOY_GetState();
+
+    switch (joystickState) {
+        case JOY_UP:
+            if (selectedButton > 1) selectedButton -= 2; // Bouton au-dessus
+            break;
+        case JOY_DOWN:
+            if (selectedButton < 2) selectedButton += 2; // Bouton en dessous
+            break;
+        case JOY_LEFT:
+            if (selectedButton % 2 == 1) selectedButton--; // Bouton à gauche
+            break;
+        case JOY_RIGHT:
+            if (selectedButton % 2 == 0) selectedButton++; // Bouton à droite
+            break;
+        case JOY_SEL:
+            // Changer la couleur du bouton sélectionné
+            buttons[selectedButton].color = LCD_COLOR_GREEN;
+            break;
+        default:
+            break;
+    }
+    HighlightButton(selectedButton);
+    HAL_Delay(200); // Anti-rebond
+}
+
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-
+int main(void) {
   /* USER CODE BEGIN 1 */
-  UART_periodic();
-  I2C_periodic();
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
+  SystemClock_Config();
 
   /* USER CODE BEGIN Init */
 
@@ -93,111 +162,64 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_FSMC_Init();
-  MX_I2C1_Init();
-  MX_I2C2_Init();
-  MX_I2S3_Init();
-  MX_QUADSPI_Init();
-  MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
   BSP_LCD_Init();
   BSP_LCD_Clear(LCD_COLOR_WHITE);
+  BSP_JOY_Init(JOY_MODE_GPIO);
 
-    BSP_LCD_DisplayStringAt(0, 240 - 65, (uint8_t *)"Axel Le MEILLEUR", CENTER_MODE);
+  /* USER CODE BEGIN 2 */
+  InitializeButtons();
   /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
+    /* USER CODE BEGIN WHILE */
+    HandleJoystick();
     /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
-  /* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 72;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 3;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 4;
+    RCC_OscInitStruct.PLL.PLLN = 72;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 3;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_HSI, RCC_MCODIV_1);
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
-/* USER CODE BEGIN 4 */
-//new function here
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+/* USER CODE BEGIN Error_Handler */
+void Error_Handler(void) {
+    __disable_irq();
+    while (1) {
+    }
 }
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
+/* USER CODE END Error_Handler */
